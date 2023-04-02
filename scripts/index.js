0906250b621cd0ffc7973543804360ce4122f8c7
AntonMikhailov1 <mikhailovanton.nsk@gmail.com>
2023-04-02 23:32:34 +0300

    ''

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,51 @@
-let page = document.querySelector('.page');
-let profile = page.querySelector('.profile');
-let popup = page.querySelector('.popup');
+const page = document.querySelector('.page');
 
-let editBtn = profile.querySelector('.profile__edit-btn');
-let profileName = profile.querySelector('.profile__name');
-let profileDisc = profile.querySelector('.profile__disc');
+const profile = page.querySelector('.profile');
 
-let popupForm = popup.querySelector('.popup__form');
-let submitBtn = popup.querySelector('.popup__submit-btn');
-let closeBtn = popup.querySelector('.popup__close-btn');
-let fieldName = popup.querySelector('.popup__field_input_name');
-let fieldDisc = popup.querySelector('.popup__field_input_discription');
+const editBtn = profile.querySelector('.profile__edit-btn');
+const addBtn = profile.querySelector('.profile__add-btn');
+const profileName = profile.querySelector('.profile__name');
+const profileDisc = profile.querySelector('.profile__disc');
 
-function popupOpen() {
-    fieldName.value = profileName.textContent;
-    fieldDisc.value = profileDisc.textContent;
+const popup = document.content.querySelector('.popup');
+const popupForm = popupTemplate.content.querySelector('.popup__form');
+const submitBtn = popupTemplate.content.querySelector('.popup__submit-btn');
+const closeBtn = popupTemplate.content.querySelector('.popup__close-btn');
 
+const initialCards = [
+  {
+    name: 'Йосемити',
+    link: '../images/yosemite.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function popupOpen(e) {
     popup.classList.add('popup_opened');
+
+    console.log(e);
 }
+
+
 
 function popupClose() {
     popup.classList.remove('popup_opened');
@@ -31,8 +59,67 @@ function popupSubmit(evt) {
     popupClose();
 }
 
-editBtn.addEventListener('click', popupOpen);
-closeBtn.addEventListener('click', popupClose);
-popupForm.addEventListener('submit', popupSubmit);
+function editBtnClick(btn) {
+    btn.addEventListener('click', popupOpen);
+    btn.addEventListener('click', popupProfile);
+}
+
+function closeBtnClick(btn) {
+    btn.addEventListener('click', popupClose);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function popupProfile() {
+//     const popupElement = popupTemplate.querySelector('.popup').cloneNode(true);
+//     popupElement.querySelector('.popup__title').textContent = 'Редактировать профиль';
+    
+//     popupElement.querySelectorAll('.popup__field').forEach((e, i) => {
+//         console.log(e);
+//         if (i === 0) {
+//             e.classList.add('popup__field_input_name');
+//             e.setAttribute('value', 'Жак Ив-Кусто');
+//             e.setAttribute('name', 'name');
+//         }  
+//         if (i === 1) {
+//             e.classList.add('popup__field_input_discription');
+//             e.setAttribute('value', 'Исследователь океана');
+//             e.setAttribute('name', 'discription');
+//         }
+//     });
+
+//     // fieldProfileInputs[0].value = profileName.textContent;
+//     // fieldProfileInputs[1].value = profileDisc.textContent;
+//     page.append(popupElement); 
+// }
+
+
+
+
+
+
+// popupForm.addEventListener('submit', popupSubmit);
 
 
